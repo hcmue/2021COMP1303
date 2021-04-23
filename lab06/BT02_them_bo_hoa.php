f167ad896538f35bfd3b61ae461dfa32119e21d6
Hien Luong <hyhien@gmail.com>
2021-04-23 08:43:50 +0700

    Lab06 - Bai tap 2

---
+++ lab06/BT02_them_bo_hoa.php
@@ -0,0 +1,28 @@
+<form action="" method="post" enctype="multipart/form-data">
+    <table>
+        <caption>THÊM BÓ HOA</caption>
+        <tr>
+            <td>Tên bó hoa</td>
+            <td>
+                <input type="text" name='TenBoHoa' />
+            </td>
+        </tr>
+        <tr>
+            <td>Giá bán</td>
+            <td>
+                <input type="number" min="0" name="GiaBan">
+            </td>
+        </tr>
+        <tr>
+            <td>Hình</td>
+            <td>
+                <input type="file" name="Hinh" id="">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <button>Thêm bó hoa</button>
+            </td>
+        </tr>
+    </table>
+</form>
