72e627b3d84e0980bdeed29329fd0951c07495a0
Hien Luong <hyhien@gmail.com>
2021-04-23 09:09:34 +0700

    Lab06 - BT02

--- lab06/BT02_them_bo_hoa.php
+++ lab06/BT02_them_bo_hoa.php
@@ -20,9 +20,33 @@
             </td>
         </tr>
         <tr>
-            <td>
+            <td colspan="2" style="text-align:center;">
                 <button>Thêm bó hoa</button>
             </td>
         </tr>
     </table>
 </form>
+<?php
+if(isset($_FILES["Hinh"]) && isset($_REQUEST["TenBoHoa"]) 
+    && isset($_REQUEST["GiaBan"])){
+    //upload file vào thư mục hoa
+    if($_FILES["Hinh"]["error"] == 0){
+        if(move_uploaded_file($_FILES["Hinh"]["tmp_name"], 
+            "./hoa/{$_FILES['Hinh']['name']}")){
+            $chuoi = "/*{$_REQUEST['TenBoHoa']}|{$_REQUEST['GiaBan']}|{$_FILES['Hinh']['name']}";
+            $filehandle = fopen("hoaxuan.txt", "a");
+            fwrite($filehandle, $chuoi);
+            fclose($filehandle);
+            $chuoi_xuat = <<< EOD
+            <div style="text-align:center;">
+                Đã ghi file thành công!<br>
+                <img src="./hoa/{$_FILES['Hinh']['name']}" /><br>
+                {$_REQUEST['TenBoHoa']} - {$_REQUEST['GiaBan']}<br>
+                <a href="BT02_doc_bo_hoa.php">Xem danh sách bó hoa</a>
+            </div>
+EOD;
+            echo $chuoi_xuat;
+        }
+    }
+}
+?>
